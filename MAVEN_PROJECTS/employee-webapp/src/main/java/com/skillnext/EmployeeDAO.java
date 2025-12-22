1c743f8a65fd671797b021fad4d09a4e03330d0a
vonisaivaishnavi <saivaishnavivoni@gmail.com>
2025-12-22 10:19:35 +0530

    employee-webapp

---
+++ MAVEN_PROJECTS/employee-webapp/src/main/java/com/skillnext/EmployeeDAO.java
@@ -0,0 +1,34 @@
+package com.skillnext;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
+public class EmployeeDAO {
+
+    public static void save(Employee emp) {
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            Connection con = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/skillnext_db",
+                "root",
+                "1833986"
+            );
+
+            String sql = "INSERT INTO employee(name,email,salary) VALUES(?,?,?)";
+            PreparedStatement ps = con.prepareStatement(sql);
+
+            ps.setString(1, emp.getName());
+            ps.setString(2, emp.getEmail());
+            ps.setDouble(3, emp.getSalary());
+
+            ps.executeUpdate();
+            con.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
